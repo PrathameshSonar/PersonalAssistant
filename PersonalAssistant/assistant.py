d64aca1ff5825cbfe93d6f8209cfcda46548ff05
PrathameshSonar <pmsonar2000@gmail.com>
2020-10-28 09:56:10 +0530

    implemented locate city

--- PersonalAssistant/assistant.py
+++ PersonalAssistant/assistant.py
@@ -24,9 +24,9 @@ import subprocess
 
 def speak(audioString):
     print(audioString)
-    tts = gTTS(text=audioString, lang='en')
+    tts = gTTS(text=audioString, lang="en")
     r = random.randint(1, 20000000)
-    audio_file = 'audio' + str(r) + '.mp3'
+    audio_file = "audio" + str(r) + ".mp3"
     tts.save(audio_file)
     playsound.playsound(audio_file)
     #os.system("mpg321 audio.mp3")
@@ -76,58 +76,58 @@ def jarvis(data):
 
     elif "exit" in data or "close" in data or "goodbye" in data:
         speak(
-            'I feeling very sweet after meeting with you but you are going! i am very sad')
+            "I feeling very sweet after meeting with you but you are going! i am very sad")
         exit()
 
     elif "send mail" in data or "email" in data:
-        speak('Enter a Email ID of User who u want to send mail')
-        email_id = input('\n>>')
-        speak('Tell a subject for mail : ')
+        speak("Enter a Email ID of User who u want to send mail")
+        email_id = input("\n>>")
+        speak("Tell a subject for mail : ")
         email_subject = recordAudio()
-        speak('Tell a Email Body : ')
+        speak("Tell a Email Body : ")
         email_body = recordAudio()
         send_mail(text=email_body, subject=email_subject, to_emails=[email_id])
-        speak('Mail sent successfully')
+        speak("Mail sent successfully")
 
     elif "check for any new mail" in data:
         new_mail = get_inbox()
-        speak('you have mail from ' + new_mail)
+        speak("you have mail from " + new_mail)
 
     elif "schedule meeting" in data:
-        speak('Enter date in DD/MM/YYYY format')
-        dt_string = input('\nDate (DD/MM/YYYY): ')
+        speak("Enter date in DD/MM/YYYY format")
+        dt_string = input("\nDate (DD/MM/YYYY): ")
 
-        speak('Enter time in HH:MM format')
-        t = input('\nHH:MM : ')
+        speak("Enter time in HH:MM format")
+        t = input("\nHH:MM : ")
 
-        dt_string = dt_string + ' ' + t
+        dt_string = dt_string + " " + t
         dt = datetime.datetime.strptime(dt_string, "%d/%m/%Y %H:%M")
         print(dt)
 
         currentDate = datetime.datetime.today()
 
         if dt <= currentDate:
-            speak('Given date should be greater than todays date')
+            speak("Given date should be greater than todays date")
         else:
-            speak('Tell a summary for meeting : ')
+            speak("Tell a summary for meeting : ")
             summary = recordAudio()
             create_event(dt, summary)
-            speak('Meeting scheduled successfully')
+            speak("Meeting scheduled successfully")
 
     elif "check for any meeting scheduled" in data:
         events = list_events()
         for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            speak('you have meeting scheduled on ' + start)
-            print(start, event['summary'])
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            speak("you have meeting scheduled on " + start)
+            print(start, event["summary"])
 
     elif "shutdown" in data:
         speak("Hold On a Sec ! Your system is on its way to shut down")
-        os.system('shutdown -s')
+        os.system("shutdown -s")
 
     elif "hibernate" in data or "sleep" in data:
         speak("Hibernating")
-        os.system('shutdown.exe /h")
+        os.system("shutdown.exe /h")
 
     elif "change name" in data or "name change" in data:
         speak("tell a new name")
@@ -136,55 +136,55 @@ def jarvis(data):
             setName(name)
             speak(f"okay, i will remember that {name}")
 
-    elif 'open youtube' in data or "open video online" in data:
+    elif "open youtube" in data or "open video online" in data:
         webbrowser.open("www.youtube.com")
         speak("opening youtube")
 
-    elif 'open github' in data:
+    elif "open github" in data:
         webbrowser.open("https://www.github.com")
         speak("opening github")
 
-    elif 'open facebook' in data:
+    elif "open facebook" in data:
         webbrowser.open("https://www.facebook.com")
         speak("opening facebook")
 
-    elif 'open instagram' in data:
+    elif "open instagram" in data:
         webbrowser.open("https://www.instagram.com")
         speak("opening instagram")
 
-    elif 'open google' in data:
+    elif "open google" in data:
         webbrowser.open("https://www.google.com")
         speak("opening google")
 
-    elif 'open yahoo' in data:
+    elif "open yahoo" in data:
         webbrowser.open("https://www.yahoo.com")
         speak("opening yahoo")
 
-    elif 'open amazon' in data or 'shop online' in data:
+    elif "open amazon" in data or "shop online" in data:
         webbrowser.open("https://www.amazon.com")
         speak("opening amazon")
 
-    elif 'open flipkart' in data:
+    elif "open flipkart" in data:
         webbrowser.open("https://www.flipkart.com")
         speak("opening flipkart")
 
-    elif "what\'s up" in data or 'how are you' in data:
-        stMsgs = ['Just doing my thing!', 'I am fine!', 'Nice!',
-                  'I am nice and full of energy', 'i am okey ! How are you']
+    elif "what\"s up" in data or "how are you" in data:
+        stMsgs = ["Just doing my thing!", "I am fine!", "Nice!",
+                  "I am nice and full of energy", "i am okey ! How are you"]
         ans_q = random.choice(stMsgs)
         speak(ans_q)
         user_reply = recordAudio()
-        if 'fine' in user_reply or 'okay' in user_reply:
-            speak('okay..')
-        elif 'not' in user_reply or 'sad' in user_reply or 'upset' in user_reply:
-            speak('oh sorry..')
+        if "fine" in user_reply or "okay" in user_reply:
+            speak("okay..")
+        elif "not" in user_reply or "sad" in user_reply or "upset" in user_reply:
+            speak("oh sorry..")
 
     elif "who are you" in data or "about you" in data or "your details" in data:
-        speak('i am your assistant')
+        speak("i am your assistant")
 
     elif "hey buddy" in data or "hello buddy" in data:
         name = getName()
-        speak('Hello ' + name + ' How may i help you')
+        speak("Hello " + name + " How may i help you")
 
     elif "open calculator" in data or "calculator" in data:
         speak("opening calculator")
@@ -194,22 +194,30 @@ def jarvis(data):
         speak("opening notepad")
         call(["notepad.exe"])
 
-    elif "search for" in data and 'youtube' not in data:
+    elif "search for" in data and "youtube" not in data:
         search_term = data.split("for")[-1]
         url = f"https://google.com/search?q={search_term}"
         webbrowser.get().open(url)
-        speak(f'Here is what I found for {search_term} on google')
+        speak(f"Here is what I found for {search_term} on google")
 
     elif "youtube" in data:
         search_term = data.split("for")[-1]
         url = f"https://www.youtube.com/results?search_query={search_term}"
         webbrowser.get().open(url)
-        speak(f'Here is what I found for {search_term}')
+        speak(f"Here is what I found for {search_term}")
 
     elif "don't listen" in data or "stop listening" in data:
         speak("for how much time you want to stop assistant from listening commands")
         a = int(recordAudio())
         time.sleep(a)
+
+    elif "locate " in data:
+        search_term = data.split(" ")[-1]
+        print(search_term)
+        url = f"https://www.google.com/maps/place/{search_term}"
+        webbrowser.get().open(url)
+        speak(f"Here is what I found for {search_term} on google map")
+        
 
 
     # if "show yahoo stocks":
